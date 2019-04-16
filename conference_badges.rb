e0e5a1f962bfd5b30c6429f2f06b18096252b5dd
Greg Morgan <gregory.morgan@flatironschool.com>
2019-04-15 18:00:40 -0700

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def badge_maker(name)
+   "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    ar=[]
+    attendees.each do |name|
+        ar << "Hello, my name is #{name}."
+    end
+    ar
+end
+
+def assign_rooms(attendees)
+    ar=[]
+    attendees.each_with_index.map do |name, index|
+        ar << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    end
+    ar
+end
+
+def printer(attendees)
+   batch_badge_creator(attendees).each do |name|
+    puts name
+   end
+   assign_rooms(attendees).each do |n|
+    puts n
+   end
+end
